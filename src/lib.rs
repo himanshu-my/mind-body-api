15e6d618f18ffa23be73b5533a48780b095f8a63
Himanshu Rathore <himanshu.rathore@moreyeahs.in>
2023-05-01 13:22:36 +0530

    put code in main file

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,18 @@
-use actix_web::{web::ServiceConfig, get};
+use actix_web::{get, web::ServiceConfig};
 use shuttle_service::ShuttleActixWeb;
 mod countries;
 mod provinces;
-
 
 #[get("/hello")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
-#[shuttle_service::main]
-async fn actix_web(
-) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Clone + 'static> {
-    Ok(move |cfg: &mut ServiceConfig| {
-        cfg.service(countries::list_countries)
-        .service(provinces::list_provinces);
-    })
-}
+// #[shuttle_service::main]
+// async fn actix_web(
+// ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Clone + 'static> {
+//     Ok(move |cfg: &mut ServiceConfig| {
+//         cfg.service(countries::list_countries)
+//         .service(provinces::list_provinces);
+//     })
+// }
--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,12 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
-// #[shuttle_runtime::main]
-// async fn actix_web(
-// ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-//     let config = move |cfg: &mut ServiceConfig| {
-//         cfg.service(hello_world);
-//     };
-
-//     Ok(config.into())
-// }
-
-#[shuttle_service::main]
-async fn actix_web(
-) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Clone + 'static> {
-    Ok(move |cfg: &mut ServiceConfig| {
+#[shuttle_runtime::main]
+async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut ServiceConfig| {
         cfg.service(countries::list_countries)
             .service(provinces::list_provinces);
-    })
+    };
+
+    Ok(config.into())
 }

7ec295465cbdef74ab12612b868cb199246374e7
Himanshu Rathore <himanshu.rathore@moreyeahs.in>
2023-05-01 13:00:09 +0530

    put code in main file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+mod countries;
+mod provinces;
 
 #[get("/hello")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
-#[shuttle_runtime::main]
-async fn actix_web(
-) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
-    };
+// #[shuttle_runtime::main]
+// async fn actix_web(
+// ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+//     let config = move |cfg: &mut ServiceConfig| {
+//         cfg.service(hello_world);
+//     };
 
-    Ok(config.into())
+//     Ok(config.into())
+// }
+
+#[shuttle_service::main]
+async fn actix_web(
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Clone + 'static> {
+    Ok(move |cfg: &mut ServiceConfig| {
+        cfg.service(countries::list_countries)
+            .service(provinces::list_provinces);
+    })
 }
